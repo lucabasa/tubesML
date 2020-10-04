7cb31c186ce0821d2fa672c39b04ebf3458a0037
lucabasa <luca.basanisi@gmail.com>
2020-10-04 21:23:56 +0200

    base transformer

---
+++ source/base.py
@@ -0,0 +1,26 @@
+__author__ = 'lucabasa'
+__version__ = '0.0.1'
+__status__ = 'development'
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class BaseTransformer(BaseEstimator, TransformerMixin):
+    
+    def __init__(self):
+        self.columns = [] # useful to well behave with FeatureUnion
+        
+        
+    def fit(self, X, y=None):
+        self.columns = [] # for safety if there are multiple fits
+        return self
+        
+        
+    def transform(self, X, y=None):
+        self.columns = X.columns
+        return X
+     
+        
+    def get_feature_names(self):
+        return self.columns
+
