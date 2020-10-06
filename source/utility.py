e3e71d5391a11caa50869bbeefd98bec5fe557d5
lucabasa <luca.basanisi@gmail.com>
2020-10-06 21:43:02 +0200

    added featureunion

--- source/utility.py
+++ source/utility.py
@@ -29,3 +29,44 @@ class DtypeSel(BaseTransformer):
             cat_cols = X.columns[X.dtypes == object].tolist()
             X_tr = X[cat_cols]
         return X_tr
+    
+    
+class FeatureUnionDf(BaseTransformer):
+    '''
+    Wrapper of FeatureUnion but returning a Dataframe, 
+    the column order follows the concatenation done by FeatureUnion
+
+    transformer_list: list of Pipelines
+
+    '''
+    def __init__(self, transformer_list, n_jobs=None, transformer_weights=None, verbose=False):
+        super.__init__()
+        self.transformer_list = transformer_list
+        self.n_jobs = n_jobs
+        self.transformer_weights = transformer_weights
+        self.verbose = verbose  # these are necessary to work inside of GridSearch or similar
+        self.feat_un = FeatureUnion(self.transformer_list, 
+                                    n_jobs=self.n_jobs, 
+                                    transformer_weights=self.transformer_weights, 
+                                    verbose=self.verbose)
+    @reset_columns    
+    def fit(self, X, y=None):
+        self.feat_un.fit(X, y)
+        return self
+    
+    @self_columns
+    def transform(self, X, y=None):
+        X_tr = self.feat_un.transform(X)
+        columns = []
+        
+        for trsnf in self.transformer_list:
+            cols = trsnf[1].steps[-1][1].get_feature_names()
+            columns += list(cols)
+
+        X_tr = pd.DataFrame(X_tr, index=X.index, columns=columns)
+        
+        return X_tr
+
+    
+    def get_params(self, deep=True):  # necessary to well behave in GridSearch
+        return self.feat_un.get_params(deep=deep)
