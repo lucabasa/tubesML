ab28d6c230f805ec2029e8131d95f8e51542031c
lucabasa <luca.basanisi@gmail.com>
2021-01-31 17:17:26 +0100

    cv score started

--- tubesml/model_selection.py
+++ tubesml/model_selection.py
@@ -1,10 +1,12 @@
 __author__ = 'lucabasa'
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 __status__ = 'development'
 
 import pandas as pd
 
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+
+import report as rp
 
 
 def grid_search(data, target, estimator, param_grid, scoring, cv, random=False):
@@ -37,3 +39,50 @@ def grid_search(data, target, estimator, param_grid, scoring, cv, random=False):
     result = result[params + ['mean_train_score', 'std_train_score', 'mean_test_score', 'std_test_score'] + times]
     
     return result, grid.best_params_, grid.best_estimator_
+
+
+def cv_score(data, target, estimator, cv, imp_coef=False, predict_proba=False):
+    '''
+    Train and test a pipeline in kfold cross validation
+    Returns the oof predictions for the entire train set and a dataframe with the
+    coefficients or feature importances, averaged across the folds, with standard deviation
+
+    todo: report on the average score and variance too
+    '''
+    oof = np.zeros(len(data))
+    train = data.copy()
+    
+    feat_df = pd.DataFrame()
+    
+    for n_fold, (train_index, test_index) in enumerate(cv.split(train.values)):
+            
+        trn_data = train.iloc[train_index][:]
+        val_data = train.iloc[test_index][:]
+        
+        trn_target = target.iloc[train_index].values.ravel()
+        val_target = target.iloc[test_index].values.ravel()
+        
+        estimator.fit(trn_data, trn_target)
+        
+        if predict_proba:
+            oof[test_index] = estimator.predict_proba(val_data)[:,1]
+        else:
+            oof[test_index] = estimator.predict(val_data).ravel()
+
+        if imp_coef:
+            try:
+                fold_df = rp.get_coef(estimator)
+            except (AttributeError, KeyError):
+                fold_df = rp.get_feature_importance(estimator)
+                
+            fold_df['fold'] = n_fold + 1
+            feat_df = pd.concat([feat_df, fold_df], axis=0)
+       
+    if imp_coef:
+        feat_df = feat_df.groupby('feat')['score'].agg(['mean', 'std'])
+        feat_df['abs_sco'] = (abs(feat_df['mean']))
+        feat_df = feat_df.sort_values(by=['abs_sco'],ascending=False)
+        del feat_df['abs_sco']
+        return oof, feat_df
+    else:    
+        return oof
