48ad50a0ae284a303da31f25a1b2336f37aff270
lucabasa <luca.basanisi@gmail.com>
2021-02-21 11:09:18 +0100

    updated init

--- tubesml/__init__.py
+++ tubesml/__init__.py
@@ -3,5 +3,9 @@ from tubesml.scale import DfScaler
 from tubesml.dummy import Dummify
 from tubesml.utility import DtypeSel, FeatureUnionDf
 from tubesml.model_selection import grid_search, cv_score
+from tubesml.report import (get_coef, get_feature_importance, plot_regression_predictions, 
+                            plot_learning_curve, plot_feat_imp, plot_pdp, plot_partial_dependence)
 
-__all__ = ['DfImputer', 'DfScaler', 'Dummify', 'DtypeSel', 'FeatureUnionDf', 'grid_search', 'cv_score']
+__all__ = ['DfImputer', 'DfScaler', 'Dummify', 'DtypeSel', 'FeatureUnionDf', 
+           'grid_search', 'cv_score', 'get_coef', 'get_feature_importance', 'plot_regression_predictions', 
+           'plot_learning_curve', 'plot_feat_imp, plot_pdp', 'plot_partial_dependence']
