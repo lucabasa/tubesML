8c9604650dea90a72c6c4a057784c2e078ba9fe2
lucabasa <luca.basanisi@gmail.com>
2021-02-21 10:33:13 +0100

    plotting learning curves

--- tubesml/report.py
+++ tubesml/report.py
@@ -91,3 +91,79 @@ def plot_regression_predictions(data, true_label, pred_label, hue=None, savename
         plt.show()
     else:
         plt.show()
+
+
+def plot_learning_curve(estimator, X, y, scoring=None, ylim=None, cv=None,
+                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 10), title=None):
+    '''
+    Plot learning curve and scalability of the model
+    It may create issues when both the estimator and this function have n_jobs>1
+    '''
+    
+    fig, ax = plt.subplots(2, 2, figsize=(12, 12))
+
+    train_sizes, train_scores, test_scores, fit_times, score_times = \
+        learning_curve(estimator, X, y, cv=cv, n_jobs=n_jobs,
+                       scoring=scoring,
+                       train_sizes=train_sizes,
+                       return_times=True)
+    
+    if not scoring is None:
+        if 'neg' in scoring:
+            train_scores = -train_scores
+            test_scores = -test_scores
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1) / np.sqrt(cv.get_n_splits() - 1)  # std of the mean, unbiased
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1) / np.sqrt(cv.get_n_splits() - 1)
+    fit_times_mean = np.mean(fit_times, axis=1)
+    fit_times_std = np.std(fit_times, axis=1) / np.sqrt(cv.get_n_splits() - 1)
+    score_times_mean = np.mean(score_times, axis=1)
+    score_times_std = np.std(score_times, axis=1) / np.sqrt(cv.get_n_splits() - 1)
+
+    # Plot learning curve
+    ax[0][0].fill_between(train_sizes, train_scores_mean - train_scores_std,
+                         train_scores_mean + train_scores_std, alpha=0.1,
+                         color="r")
+    ax[0][0].fill_between(train_sizes, test_scores_mean - test_scores_std,
+                         test_scores_mean + test_scores_std, alpha=0.1,
+                         color="g")
+    ax[0][0].plot(train_sizes, train_scores_mean, 'o-', color="r",
+                 label="Training score")
+    ax[0][0].plot(train_sizes, test_scores_mean, 'o-', color="g",
+                 label="Cross-validation score")
+    ax[0][0].legend(loc="best")
+    ax[0][0].set_title('Train and test scores', fontsize=14)
+    if ylim is not None:
+        ax[0][0].set_ylim(*ylim)
+    ax[0][0].set_xlabel("Training examples")
+    ax[0][0].set_ylabel("Score")
+
+    # Plot n_samples vs fit_times
+    ax[0][1].plot(train_sizes, fit_times_mean, 'o-')
+    ax[0][1].fill_between(train_sizes, fit_times_mean - fit_times_std,
+                         fit_times_mean + fit_times_std, alpha=0.1)
+    ax[0][1].set_xlabel("Training examples")
+    ax[0][1].set_ylabel("fit_times")
+    ax[0][1].set_title("Scalability of the model", fontsize=14)
+
+    # Plot fit_time vs score
+    ax[1][0].plot(fit_times_mean, test_scores_mean, 'o-')
+    ax[1][0].fill_between(fit_times_mean, test_scores_mean - test_scores_std,
+                         test_scores_mean + test_scores_std, alpha=0.1)
+    ax[1][0].set_xlabel("fit_times")
+    ax[1][0].set_ylabel("Score")
+    ax[1][0].set_title("Fit time vs test score", fontsize=14)
+    
+    # Plot fit_time vs fit_score
+    ax[1][1].plot(fit_times_mean, train_scores_mean, 'o-')
+    ax[1][1].fill_between(fit_times_mean, train_scores_mean - train_scores_std,
+                         train_scores_mean + train_scores_std, alpha=0.1)
+    ax[1][1].set_xlabel("fit_times")
+    ax[1][1].set_ylabel("Score")
+    ax[1][1].set_title("Fit time vs train score", fontsize=14)
+    
+    if title is not None:
+        fig.suptitle(f'{title}', fontsize=18)
+    
+    plt.show()       
