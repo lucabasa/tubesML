33f8c03d18e9042ccbba9e773c9e1e6d6c744fc5
lucabasa <luca.basanisi@gmail.com>
2021-02-23 22:59:16 +0100

    updated init

--- tubesml/__init__.py
+++ tubesml/__init__.py
@@ -2,8 +2,9 @@ from tubesml.clean import DfImputer
 from tubesml.scale import DfScaler
 from tubesml.dummy import Dummify
 from tubesml.pca import PCADf
+from tubesml.poly import PolynomialDf
 from tubesml.utility import DtypeSel, FeatureUnionDf
 from tubesml.model_selection import grid_search, cv_score
 
-__all__ = ['DfImputer', 'DfScaler', 'Dummify', 'PCADf',
+__all__ = ['DfImputer', 'DfScaler', 'Dummify', 'PCADf', 'PolynomialDf',
            'DtypeSel', 'FeatureUnionDf', 'grid_search', 'cv_score']
