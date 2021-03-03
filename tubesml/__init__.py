4bf376e47785717b961e6d669effd0a6ea027b67
lucabasa <luca.basanisi@gmail.com>
2021-03-03 22:41:25 +0100

    updated init

--- tubesml/__init__.py
+++ tubesml/__init__.py
@@ -5,13 +5,13 @@ from tubesml.encoders import TargetEncoder
 from tubesml.pca import DfPCA
 from tubesml.poly import DfPolynomial
 from tubesml.utility import DtypeSel, FeatureUnionDf
-from tubesml.model_selection import grid_search, cv_score
+from tubesml.model_selection import grid_search, cv_score, make_test
 from tubesml.report import (get_coef, get_feature_importance, plot_regression_predictions, 
                             plot_learning_curve, plot_feat_imp, plot_pdp, plot_partial_dependence)
 from tubesml.explore import (list_missing, plot_correlations, plot_distribution, plot_bivariate, 
                              corr_target, find_cats, segm_target)
 
 __all__ = ['DfImputer', 'DfScaler', 'Dummify', 'TargetEncoder', 'DfPCA', 'DfPolynomial',
-           'DtypeSel', 'FeatureUnionDf', 'grid_search', 'cv_score', 'get_coef', 'get_feature_importance', 
+           'DtypeSel', 'FeatureUnionDf', 'grid_search', 'cv_score', 'make_test', 'get_coef', 'get_feature_importance', 
            'plot_regression_predictions', 'plot_learning_curve', 'plot_feat_imp, plot_pdp', 'plot_partial_dependence', 
            'list_missing', 'plot_correlations', 'plot_distribution', 'plot_bivariate', 'corr_target', 'find_cats', 'segm_target']
