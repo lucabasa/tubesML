5a1b4824f1d70ce4e9d69dc2e9e233a9af5ab410
lucabasa <luca.basanisi@gmail.com>
2022-09-24 14:33:16 +0200

    updated no warning cv score tests

--- tests/test_cvscore.py
+++ tests/test_cvscore.py
@@ -1,5 +1,6 @@
 import tubesml as tml
 import pytest
+import warnings
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
@@ -56,9 +57,9 @@ def test_cvscore(predict_proba):
 
     kfold = KFold(n_splits=3)
     
-    with pytest.warns(None) as record:
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
         res, _ = tml.cv_score(df_1, y, full_pipe, cv=kfold, predict_proba=predict_proba)
-    assert len(record) == 0
     assert len(res) == len(df_1)
     
     
@@ -73,9 +74,9 @@ def test_cvscore_nopipe():
     
     full_pipe = LogisticRegression(solver='lbfgs', multi_class='auto')
     
-    with pytest.warns(None) as record:
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
         res, _ = tml.cv_score(df_1, y, full_pipe, cv=kfold, predict_proba=True)
-    assert len(record) == 0
     assert len(res) == len(df_1)
 
 
@@ -101,9 +102,9 @@ def test_earlystopping(model):
 
     kfold = KFold(n_splits=3)
     
-    with pytest.warns(None) as record:
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
         res, res_dict = tml.cv_score(df_1, y, full_pipe, cv=kfold, early_stopping=5, eval_metric='auc', imp_coef=True)
-    assert len(record) == 0
     assert len(res) == len(df_1)
     assert len(res_dict['iterations']) == 3  # one per fold
 
@@ -133,9 +134,9 @@ def test_cvscore_coef_imp(model):
 
     kfold = KFold(n_splits=3)
 
-    with pytest.warns(None) as record:
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
         res, coef = tml.cv_score(df_1, y, full_pipe, cv=kfold, imp_coef=True)
-    assert len(record) == 0
     assert len(coef['feat_imp']) == df_1.shape[1]  * 2 + 45  # to account for the combinations
 
 
@@ -151,9 +152,9 @@ def test_cvscore_nopipeline(model):
     
     kfold = KFold(n_splits=3)
     
-    with pytest.warns(None) as record:
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
         res, coef = tml.cv_score(df_1, y, model, cv=kfold, imp_coef=True)
-    assert len(record) == 0
     assert len(res) == len(df_1)
     assert len(coef['feat_imp']) == df_1.shape[1]
     
@@ -180,25 +181,25 @@ def test_cvscore_pdp(model):
     full_pipe = Pipeline([('pipe', pipe), 
                           ('model', model)])
     
-    with pytest.warns(None) as record:
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
         res, pdp_res = tml.cv_score(df_1, y, full_pipe, cv=kfold, pdp=pdp)
-    assert len(record) == 0
     assert set(pdp_res['pdp']['feat']) == set(pdp)
     assert pdp_res['pdp']['mean'].notna().all()
     assert pdp_res['pdp']['std'].notna().all()
 
     
 def test_make_test():
-    with pytest.warns(None) as record:
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
         train, test = tml.make_test(df, 0.2, 452)
-    assert len(record) == 0
 
 
 def test_strat_test():
     df_1 = df.copy()
     df['cat'] = ['a']*50 + ['b']*50
-    with pytest.warns(None) as record:
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
         train, test = tml.make_test(df, 0.2, 452, strat_feat='cat')
-    assert len(record) == 0
     assert len(train[train['cat']=='a']) == len(train) / 2
     
