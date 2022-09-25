f80bdca484fc898481d0f31ae68135ebb2eb75c0
lucabasa <luca.basanisi@gmail.com>
2022-09-25 09:37:20 +0200

    updating version docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2021, Luca Basanisi'
 author = 'Luca Basanisi'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.6.0'
 
 
 # -- General configuration ---------------------------------------------------
