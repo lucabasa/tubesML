ff0251f1dad3214ae33ab17b8723d8e4c0be0b31
lucabasa <luca.basanisi@gmail.com>
2025-05-03 22:47:18 +0200

    only use general explainer, fewer default samples, and fixed format when needed

--- tubesml/shap_values.py
+++ tubesml/shap_values.py
@@ -3,20 +3,16 @@ import pandas as pd
 import shap
 
 
-def get_shap_values(data, model, model_type="general", sample=2000):
+def get_shap_values(data, model, sample=700, class_pos=1):
 
-    if model_type == "general":
-        expl = shap.Explainer(model, data)
-    elif model_type == "tree":
-        expl = shap.TreeExplainer(model)
-    elif model_type == "kernel":
-        expl = shap.KernelExplainer(model, data)
-    else:
-        raise ValueError("Invalid model_type")
-
+    expl = shap.Explainer(model, data)
+    
     n_samples = min(sample, len(data))
 
     shap_values = expl(data.sample(n_samples))
+
+    if len(shap_values.values.shape) == 3:
+        shap_values = _fix_format(shap_values, class_pos)
 
     return shap_values
 
@@ -30,3 +26,10 @@ def get_shap_importance(data, shap_values):
         .T.rename(columns={"mean": "shap_importance", "std": "shap_std"})
         .sort_values(by="shap_importance", ascending=False)
     )
+
+
+def _fix_format(shap_values, class_pos):
+
+    shap_values.values = shap_values.values[:, :, class_pos]
+
+    return shap_values
